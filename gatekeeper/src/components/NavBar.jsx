fbda8151e98be43bd28cb87f376d28d829c1a6c7
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2022-11-17 15:15:21 +0100

    commit

--- gatekeeper/src/components/NavBar.jsx
+++ gatekeeper/src/components/NavBar.jsx
@@ -33,9 +33,7 @@ class Nav extends Component {
   } 
     return (
         <>
-      <header>
-        <h1 className='Title'>GateKeeper</h1>
-      </header>
+        <h1 className='Title'>Gatekeeper</h1>
         <nav className="mobile-nav">
           <Link to='/adminqr' className="bloc-icon" >
           <HiIcons.HiOutlineQrcode id='adminqr' className='icons' onClick={() => selectNav("adminqr")}/>
