95ea619ad0185607f4e4cb1ace2b17499ae79e68
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2022-12-16 20:30:13 +0100

    commit

--- gatekeeper/src/components/AdminQr.jsx
+++ gatekeeper/src/components/AdminQr.jsx
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 import axios from 'axios';
+import axiosinstance from './../api/axiosApi';
 import {motion} from "framer-motion";
 
-
 function AdminQr() {
-    const [qrdata, setqrdata] = useState('Scan a QR Code');
+    const [encryptedqrdata, setencryptedqrdata] = useState('Scan a QR Code');
     return ( 
 <motion.div
     initial={{ opacity: 0, scale: 0.5}}
@@ -21,8 +21,9 @@ function AdminQr() {
                 <QrReader
                     facingMode="environment"
                     onResult={(result, error) => {
+                    axiosinstance.post('api/qrcodeverificatorapi', {encryptedqrdata: 'gAAAAABjnLU5LD68MyflBcUgs4AX12OW4gL-UcFA6Qvt1g5eblyNKZy_Tlhye7ar0y40EjSnD32LNvkAqILE1vyxcLouGFqJv4sBcapkYkTmV6Kaa3l7JEs='})
                     if (!!result) {
-                    setqrdata(result?.text);
+                    setencryptedqrdata(result?.text);
                     }
                     if (!!error) {
                     //console.info(error);
@@ -38,7 +39,7 @@ function AdminQr() {
     <br></br>
 
     <div className="result">
-        <p className="qrdata"> {qrdata} </p>
+        <p className="encryptedqrdata"> {encryptedqrdata} </p>
     </div>
 </motion.div>
      );
