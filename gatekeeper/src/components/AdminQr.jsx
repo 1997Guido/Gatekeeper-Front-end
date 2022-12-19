d507181522fcecb6e531c435d9abf611e848c8d0
Gido1997 <101527757+Gido1997@users.noreply.github.com>
2022-12-19 14:41:02 +0100

    Axios post gefixed

--- gatekeeper/src/components/AdminQr.jsx
+++ gatekeeper/src/components/AdminQr.jsx
@@ -6,9 +6,24 @@ import { QrReader } from 'react-qr-reader';
 import axios from 'axios';
 import axiosinstance from './../api/axiosApi';
 import {motion} from "framer-motion";
+import { useCookies } from 'react-cookie';
+import { useEffect } from 'react';
 
 function AdminQr() {
+    
+    let csrftoken = useCookies(['csrftoken'])
     const [encryptedqrdata, setencryptedqrdata] = useState('Scan a QR Code');
+
+    const qrVerify = () => {
+        axiosinstance.post('api/qrcodeverificatorapi', {encryptedqrdata},
+        {headers: {'X-CSRFToken': csrftoken[0].csrftoken}})
+          .then(function(response){
+            console.log(response);
+          })
+    }
+    useEffect(() => {
+        qrVerify();
+    });
     return ( 
 <motion.div
     initial={{ opacity: 0, scale: 0.5}}
@@ -21,7 +36,6 @@ function AdminQr() {
                 <QrReader
                     facingMode="environment"
                     onResult={(result, error) => {
-                    axiosinstance.post('api/qrcodeverificatorapi', {encryptedqrdata: 'gAAAAABjnLU5LD68MyflBcUgs4AX12OW4gL-UcFA6Qvt1g5eblyNKZy_Tlhye7ar0y40EjSnD32LNvkAqILE1vyxcLouGFqJv4sBcapkYkTmV6Kaa3l7JEs='})
                     if (!!result) {
                     setencryptedqrdata(result?.text);
                     }
