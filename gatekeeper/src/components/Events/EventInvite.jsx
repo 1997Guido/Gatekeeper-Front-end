93485f58978ca0570e889da8d0f874c91d2f93ce
Gido1997 <101527757+Gido1997@users.noreply.github.com>
2023-01-09 12:57:41 +0100

    Creating Invite Component

--- gatekeeper/src/components/Events/EventInvite.jsx
+++ gatekeeper/src/components/Events/EventInvite.jsx
@@ -1,69 +1,54 @@
 import React from 'react'
 import axiosInstance from '../../api/axiosApi'
 import {useState, useEffect} from 'react'
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
-
+import Select from 'react-select'
+import './../../css/EventInvite.css'
 function EventInvite(event) {
-  const [invites, setinvites] = useState([])
-  const [searchInput, setSearchInput] = useState("");
-
   const [userlist, setuserlist] = useState([{}])
+  const [invitedUsers, setInvitedUsers] = useState([{}])
+  console.log(userlist)
+  const tempList = [{}]
+  const [selectedOption, setSelectedOption] = useState(null);
   const getUserNames = async () => {
     await axiosInstance.get('/api/usernamelistviewapi')
       .then(function(response){
-        setuserlist(response.data);
+        console.log(userlist)
         console.log(response.data)
+        for (let i = 0; i < response.data.length; i++) {
+          if (tempList.includes(response.data[i].id) === false){
+          tempList.push({value: response.data[i].id, label: response.data[i].username})
+          }
+        }
+        setuserlist(tempList)
         console.log(userlist)
       })
   }
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption)
+    invitedUsers.push(selectedOption)
+    console.log(`Option selected:`, selectedOption);
+  };
 
   useEffect(() => {
     getUserNames();
+    console.log(userlist)
   }, []);
-  const handleOnSearch = (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    console.log(string, results)
-  }
-
-  const handleOnHover = (result) => {
-    // the user hovered
-    console.log(result)
-  }
-
-  const handleOnSelect = (user) => {
-    // the user selected
-    console.log(user)
-  }
-
-  const handleOnFocus = () => {
-    console.log('Focused')
-  }
-
-  const formatResult = (user) => {
-    return (
-      <>
-        <span style={{ display: 'block', textAlign: 'left' }}>id: {user.id}</span>
-        <span style={{ display: 'block', textAlign: 'left' }}>name: {user.username}</span>
-      </>
-    )
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ width: 400 }}>
-          <ReactSearchAutocomplete
-            items={userlist}
-            onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            autoFocus
-            formatResult={formatResult}
-          />
-        </div>
-      </header>
+  <>
+    <div>
+      <Select
+        defaultValue={selectedOption}
+        onChange={handleChange}
+        options={userlist}
+        isMulti={true}
+      />
     </div>
+    <div className='Invited'>
+      {invitedUsers.map((user, index) => {
+        <p>invited:{user.label}</p>
+      })}
+    </div>
+  </>
   )
 }
 
