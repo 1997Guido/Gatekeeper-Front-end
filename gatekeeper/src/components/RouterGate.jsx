f4c75fda5a4dea8c53b3f6255b7a8508d149c881
Gido1997 <101527757+Gido1997@users.noreply.github.com>
2023-01-26 11:12:19 +0100

    fix

--- gatekeeper/src/components/RouterGate.jsx
+++ gatekeeper/src/components/RouterGate.jsx
@@ -1,19 +1,18 @@
 import { Route, Routes, useLocation} from 'react-router-dom';
 import Home from './General/Home';
-import UserProfile from '../components/Profile/UserProfile';
-import UserQr from './Qr/UserQr';
-import CreateEvent from '../components/Events/CreateEvent';
-import Events from '../components/Events/Events'
-import Scanner from './Qr/Scanner'
-import Register from './Register';
-import Login from './Login';
+import UserProfile from './Profile/UserProfile';
+import UserQr from './QR/UserQr';
+import CreateEvent from './Events/CreateEvent';
+import Events from './Events/Events'
+import Scanner from '../components/QR/Scanner'
+import Register from './General/Register';
+import Login from './General/Login';
 import {AnimatePresence} from "framer-motion";
 import React from 'react';
 import PrivateRoutes from '../api/ProtectedRoutes.jsx';
 import Nav from './Other/NavBar.jsx';
-import MyEvents from '../components/Events/MyEvents';
-import SingleEvent from '../components/Events/SingleEventPage';
-import validateUserLoggedIn from '../api/ValidateUserLoggedIn';
+import MyEvents from './Events/MyEvents';
+import SingleEvent from './Events/SingleEventPage';
 
 function RouterGate() {
     const location = useLocation();
