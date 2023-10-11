f28e25f5107868b1174260f9c5b4b9da8a2877ab
Guido Erdtsieck <guidoerdtsieck@hotmail.nl>
2023-10-11 13:39:12 +0200

    fix: fixed user validation

--- gatekeeper/src/App.js
+++ gatekeeper/src/App.js
@@ -5,24 +5,10 @@ import './css/Miscellaneous/style.css'
 import './css//Miscellaneous/mNav.css'
 import HeaderFooter from "./components/Other/HeaderFooter.jsx";
 import Footer from "./components/Other/Footer.jsx";
-import { useEffect, useState } from "react";
-import validateUserLoggedIn from "./api/ValidateUserLoggedIn";
 
 
 
 function App() {
-  let [isAuth, setIsAuth] = useState(localStorage.getItem('Auth') === 'true' ? true : false);
-  useEffect(() => {
-    validateUserLoggedIn().then((res) => {
-      if(res.data === 'true'){
-        setIsAuth(true);
-        localStorage.setItem('Auth', true);
-      }else{
-        setIsAuth(false);
-        localStorage.setItem('Auth', false);
-      }
-    })
-  })
   return (
     <div className="App">
     <Router>
