17ae915081dd36532672b722573874c0bab6a844
Mike Vermeer <97081542@st.deltion.nl>
2023-12-07 12:08:41 +0100

    feat: added cards for events

--- gatekeeper/src/components/Events/EventCard.jsx
+++ gatekeeper/src/components/Events/EventCard.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as TbIcons from "react-icons/tb";
 
 const EventCard = ({ event }) => {
@@ -8,32 +8,27 @@ const EventCard = ({ event }) => {
     navigate(`/singleevent/${singleEventPk}`);
   };
 
+  const mediaURL = process.env.NODE_ENV === 'production' ? "https://guidoerdtsieck.nl" : "http://localhost:8000"
+
   return (
-    <div className="container EventContainer" key={event.pk}>
-      <div className="row">
-        <div className="col EventTitle">{event.EventTitle}</div>
+  <div className="col">
+  <div className="card h-80 w-80 mb-5 flex-grow-0 EventContainer">
+    <img className="card-img-top img-fluid" src={mediaURL + event.EventBanner}></img>
+    <div className="card-body d-flex flex-column event_body">
+      <h5 className="card-title">{event.EventTitle}</h5>
+      <p className="card-text">{event.EventDescription}</p>
+      <ul className="list-group">
+        <li className="list-group-item text-white bg-dark">{event.EventDate}</li>
+        <li className="list-group-item text-white bg-dark">{event.EventLocation}</li>
+      </ul>
+      <div className="col">
+            <TbIcons.TbFileInfo
+              className="EventInfoButton"
+              onClick={() => navigateToSingleEvent(event.pk)}
+            />
       </div>
-      <div className="row">
-        <div className="col">Organized by: {event.EventOrganizer}</div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <TbIcons.TbCalendarEvent />
-          {event.EventDate}
-        </div>
-        <div className="col">
-          <TbIcons.TbLocation />
-          {event.EventLocation}
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <TbIcons.TbFileInfo
-            className="EventInfoButton"
-            onClick={() => navigateToSingleEvent(event.pk)}
-          />
-        </div>
-      </div>
+    </div>
+    </div>
     </div>
   );
 };
